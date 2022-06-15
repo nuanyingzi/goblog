d9d80734b35f69f1c2933c7cebc80f65804ba6da
tao <shangyou>
2022-06-15 10:50:17 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -5,25 +5,24 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "<h1>这里是GoBlog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprintf(w, "此博客是用于记录编程笔记，如您有反馈和建议，请联系 "+"<a href=\"zhongtao1024@gmail.com\">zhongtao1024@gmail.com</a>")
-	} else if r.URL.Path == "/help" {
-		fmt.Fprintf(w, "这是帮助页")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "<h1>页面未找到 :( </h1>"+"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "此博客是用于记录编程笔记，如您有反馈和建议，请联系 "+"<a href=\"zhongtao1024@gmail.com\">zhongtao1024@gmail.com</a>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":3030", nil)
 }
